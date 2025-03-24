9487b0ea0c0bb11fbb9dec35822be4e1235677b5
Kuneksa <phamminhtri208@gmail.com>
2025-03-24 20:27:32 +0700

    [build-in][list]

--- buildin/list.py
+++ buildin/list.py
@@ -1,9 +1,23 @@
-list_a = [1, 2, 3, 4]
+"""
+create list
+"""
+list1 = [1, 2, 3, 4]
 
-list_b = list_a[:]  # copy list
+list2 = list1[:]  # copy list
 
-del list_b[0]  # del element with index 0
+list3 = [*range(10, 60, 10)] # range -> list
 
-if __name__ == '__main__':
-    list_b[1] = 5
-    print(list_a, list_b)
+list4 = [num for num in range(51)]
+
+list5 = list() # formal name
+
+list6 = [] # literal syntax
+"""
+list3 faster list4 
+list6 faster list5
+"""
+
+"""
+process
+"""
+del list1[0]  # del element with index 0
