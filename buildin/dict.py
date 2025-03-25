bac6dd6bf46e6491d9c0858de02866d4667d4a2f
Kuneksa <phamminhtri208@gmail.com>
2025-03-25 21:53:06 +0700

    [build-in][dict][list]

--- buildin/dict.py
+++ buildin/dict.py
@@ -3,4 +3,8 @@ y = {'b': 3, 'c': 4}
 
 if __name__ == '__main__':
     # merge mul dict
-    print({**x, **y}) # {'a': 1, 'b': 3, 'c': 4}
+    print({**x, **y})  # {'a': 1, 'b': 3, 'c': 4}
+
+    # dict comprehensions
+    dict_cph = {num: - num for num in range(5)}
+    print(dict_cph)  # {0: 0, 1: -1, 2: -2, 3: -3, 4: -4}
--- buildin/list.py
+++ buildin/list.py
@@ -16,6 +16,10 @@ list6 = [] # literal syntax
 list3 faster list4 
 list6 faster list5
 """
+nested_loop = [(num1, num2) for num1 in range(101) for num2 in range(101)]
+matrix = [[col for col in range(5)] for row in range(5)]
+
+condition_loop = [num ** 2 for num in range(51) if num % 2 == 0]
 
 """
 process
